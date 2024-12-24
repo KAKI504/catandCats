f48e3d9fb1a07e636c609f37fc69d6a4f3e3b22e
kanat <esenalievkanat.90@gmail.com>
2024-12-24 07:26:01 +0600

    Упрощена структура класса Director Удалено разделение на firstName и lastName Добавлено одно поле fullName для полного имени

---
+++ Kinopoisk/Task2/Director.java
@@ -0,0 +1,13 @@
+package kg.Kinopoisk.Task2;
+
+public class Director {
+    private final String fullName;
+
+    public Director(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
